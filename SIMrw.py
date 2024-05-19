66a259f4be5d1fadb6ab524e7a0620f651056171
salopeknet <48922773+salopeknet@users.noreply.github.com>
2024-05-19 18:15:17 +0200

    Update SIMrw.py

--- SIMrw.py
+++ SIMrw.py
@@ -4,11 +4,11 @@ import sys
 import os
 import csv
 import argparse
-import smartcard
-#from smartcard.util import toBytes, toASCIIString, padd
-#from smartcard.System import readers
-#from smartcard.CardConnectionObserver import ConsoleCardConnectionObserver
-#from smartcard.Exceptions import NoReadersException, CardConnectionException
+#import smartcard
+from smartcard.util import toBytes, toASCIIString, padd
+from smartcard.System import readers
+from smartcard.CardConnectionObserver import ConsoleCardConnectionObserver
+from smartcard.Exceptions import NoReadersException, CardConnectionException
 
 # Version information
 version = "0.1.0"
